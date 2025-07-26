fd9253d0b2d5b838697f62bc366a74bc80daf506
Tanvi Patel <8438tanvipatel@gmail.com>
2025-07-26 19:09:03 +0530

    final commit

--- product card-2/my-app/README.md
+++
@@ -1,1 +0,0 @@
-video link : (https://drive.google.com/drive/folders/1M5KSKmjLIHM9Z7yH69Gwz6GbZ18whz1O?usp=sharing)
--- src/App.jsx
+++ src/App.jsx
@@ -8,45 +8,49 @@ import { FaGithub } from "react-icons/fa";
 
 function App() {
   return (
-    <div className="container d-flex justify-content-between mt-5">
-      <div className="information ms-2">
-        <h1
-          className="my-5"
-          style={{
-            fontFamily: "'Roboto', sans-serif",
-            fontWeight: "500",
-          }}
-        >
-          Hi, Hi, I'm John Deo.
-        </h1>
-        <p style={{ lineHeight: "22px", margin: "30px 0", color: "#9f9f9fff" }}>
-          A freelance Web developer from London. I convert custom <br /> web
-          designs to bootstrap templates. <br /> <br />I make YouTube videos and
-          write Blog.
-        </p>
+    <div className="container mt-5 main ">
+      <div className="row align-items-center">
+        <div className="information col-lg-6 col-md-12 mb-4 mb-lg-0 text-center text-lg-start px-4">
+          <h1
+            className="my-5"
+            style={{
+              fontFamily: "'Roboto', sans-serif",
+              fontWeight: "500",
+            }}
+          >
+            Hi, Hi, I'm John Deo.
+          </h1>
+          <p
+            style={{ lineHeight: "22px", margin: "30px 0", color: "#9f9f9fff" }}
+          >
+            A freelance Web developer from London. I convert custom <br /> web
+            designs to bootstrap templates. <br /> <br />I make YouTube videos
+            and write Blog.
+          </p>
 
-        <Button variant="primary" size="lg" className="rounded-pill my-2">
-          I'm Available
-        </Button>
+          <Button variant="primary" size="lg" className="rounded-pill my-2 btn">
+            I'm Available
+          </Button>
 
-        <div
-          className="icons my-4"
-          style={{ color: "#9f9f9fff", display: "flex", gap: "10px" }}
-        >
-          <FaFacebookF />
-          <FaTwitter />
-          <AiFillInstagram />
-          <FaGithub />
+          <div
+            className="icons my-4"
+            style={{ color: "#9f9f9fff", display: "flex", gap: "10px" }}
+          >
+            <FaFacebookF />
+            <FaTwitter />
+            <AiFillInstagram />
+            <FaGithub />
+          </div>
         </div>
-      </div>
 
-      <div className="image">
-        <img
-          src="src\assets\jhon_deo.png"
-          alt=""
-          height={"503px"}
-          style={{ filter: "none !impotant" }}
-        />
+        <div className="image col-lg-6 col-md-12 text-center">
+          <img
+            src="src\assets\jhon_deo.png"
+            alt=""
+            height={"420px"}
+            style={{ filter: "none !impotant" }}
+          />
+        </div>
       </div>
     </div>
   );
