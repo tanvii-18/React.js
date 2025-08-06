0b51ac87ee2da4f29bd91897c64fa35ac93adbc1
tanvii-18 <8438tanvipatel@gmail.com>
2025-08-06 09:19:20 +0530

    06-08-25

---
+++ redux-counter/src/components/Counter.jsx
@@ -0,0 +1,17 @@
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "../features/counterSlice";
+
+function Counter() {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+    </div>
+  );
+}
+
+export default Counter;
---
+++ redux-counter/src/features/counterSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  value: 0,
+};
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.value += 1;
+    },
+    decrement: (state) => {
+      if (state.value <= 0) {
+        state.value = 0;
+      } else {
+        state.value -= 1;
+      }
+    },
+  },
+});
+
+export const { increment, decrement } = counterSlice.actions;
+export default counterSlice.reducer;
