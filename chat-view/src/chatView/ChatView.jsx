f61e802b2262daef874643534f476fa0a913982e
tanvii-18 <8438tanvipatel@gmail.com>
2025-09-14 21:01:14 +0530

    ....

--- chat-view/src/chatView/ChatView.jsx
+++ chat-view/src/chatView/ChatView.jsx
@@ -3,10 +3,10 @@ import "../chatView/chatview.css";
 
 function ChatView() {
   return (
-    <div className="mainContiner">
+    <div className="flex gap-5">
       {/* <h1>Hello</h1> */}
-      <div className="box"></div>
-      <div className="box2"></div>
+      <div className="h-140 w-80 bg-amber-600 rounded-2xl"></div>
+      <div className="h-140 w-80 bg-amber-500 rounded-2xl"></div>
     </div>
   );
 }
