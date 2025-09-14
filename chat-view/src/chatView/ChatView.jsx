a3226fe97d3807ad8d25beb3cf9675c33d9e87b1
tanvii-18 <8438tanvipatel@gmail.com>
2025-09-14 19:50:42 +0530

    14-9-25

---
+++ chat-view/src/chatView/ChatView.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../chatView/chatview.css";
+
+function ChatView() {
+  return (
+    <div className="mainContiner">
+      {/* <h1>Hello</h1> */}
+      <div className="box"></div>
+      <div className="box2"></div>
+    </div>
+  );
+}
+
+export default ChatView;
