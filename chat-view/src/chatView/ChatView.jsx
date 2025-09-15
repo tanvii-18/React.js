6b230fe8be81ffede755bcc9ce37a6b2d6251cc9
tanvii-18 <8438tanvipatel@gmail.com>
2025-09-15 09:20:46 +0530

    15-9-25

--- chat-view/src/chatView/ChatView.jsx
+++ chat-view/src/chatView/ChatView.jsx
@@ -3,10 +3,14 @@ import "../chatView/chatview.css";
 
 function ChatView() {
   return (
-    <div className="flex gap-5">
+    <div className="flex justify-center gap-5 my-4">
       {/* <h1>Hello</h1> */}
-      <div className="h-140 w-80 bg-amber-600 rounded-2xl"></div>
-      <div className="h-140 w-80 bg-amber-500 rounded-2xl"></div>
+      <div className="h-130 w-80 rounded-2xl bg-[radial-gradient(circle_at_bottom_center,rgba(26,16,61,0.6)_20%,rgba(90,7,168,0.4)_60%,rgba(26,16,61,0.6)_90%)]">
+        Box1
+      </div>
+      <div className="h-130 w-80 rounded-2xl bg-[radial-gradient(circle_at_bottom_center,rgba(26,16,61,0.6)_20%,rgba(90,7,168,0.4)_60%,rgba(26,16,61,0.6)_90%)]">
+        Box2
+      </div>
     </div>
   );
 }
